09f50d0aab7b4782aedf2f7426a0ab0a587b2595
shiyunsun <138115163+shiyunsun@users.noreply.github.com>
2023-07-04 07:27:57 -0400

    Skipped UCLA header in the iframe

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
 $(document).ready(function(){
-    var myIframe = document.getElementById('news');
-    myIframe.onload = function () {
-    myIframe.contentWindow.scrollTo(0, 1000);
-    }
+    $('#news').scrollTop(590)
   });
