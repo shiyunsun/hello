159b279a07e6c33722337ab07b6b60f82ad1afed
shiyunsun <138115163+shiyunsun@users.noreply.github.com>
2023-07-05 09:32:21 -0400

    Courses added

--- script.js
+++ script.js
@@ -1,3 +1,42 @@
 $(document).ready(function(){
-    $('#news').scrollTop(590)
+    var button1clicked = false;
+    var button2clicked = true;
+    var button3clicked = true;
+
+    $('#news').scrollTop(590);
+    $('#button1').click(function(){
+        if (! button1clicked) {
+        $('#button1').text("+");
+        button1clicked = true;
+        $('#course1').hide();
+        } else {
+        $('#button1').text("-");  
+        button1clicked = false;
+        $('#course1').show();
+        }
+    })
+
+    $('#button2').click(function(){
+        if (! button2clicked) {
+        $('#button2').text("+");
+        button2clicked = true;
+        $('#course2').hide();
+        } else {
+        $('#button2').text("-");  
+        button2clicked = false;
+        $('#course2').show();
+        }
+    })
+
+    $('#button3').click(function(){
+        if (! button3clicked) {
+        $('#button3').text("+");
+        button3clicked = true;
+        $('#course3').hide();
+        } else {
+        $('#button3').text("-");  
+        button3clicked = false;
+        $('#course3').show();
+        }
+    })
   });
